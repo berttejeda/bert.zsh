fc9fef9483f9647e4b98fd5c29d18486b607ed29
Bert Tejeda <etejeda@tecknicos.com>
2023-05-22 21:17:28 -0400

    Added new function - adb.pull

--- android.functions.sh
+++ android.functions.sh
@@ -1,5 +1,7 @@
 adb.install () { adb install $1 ;}
+
 adb.push () { adb push $1 $2 ;}
+
 android.backup.personal () { 
 	folder_root="/storage/emulated/0/"
 	folders=$(adb shell ls $folder_root | grep -ie 'dcim\|pictures\|video\|download')
@@ -7,4 +9,49 @@ android.backup.personal () {
 		echo adb shell "ls -R ${folder_root}${folder}" | grep -vi '.*thumb*' | tr '\r' ' ' | while read file; do adb pull $file;done
 	done
 }
-#@ android
+
+adb.pull () {
+
+if [[ ($# -lt 1) || ("$*" =~ ".*--help.*") ]];then 
+  show_help $funcstack[1]
+  return
+fi
+
+local PREFIX=eval
+
+for arg in "${@}";do
+  shift
+  if [[ "$arg" =~ '^--source-dir$|^-s$|@The source directory to pull files from - required' ]]; then local source_dir=$1;continue;fi
+  if [[ "$arg" =~ '^--destination_dir$|^-d$|@The destination directory to store the pulled files - required' ]]; then local clip_seek_time=$1;continue;fi
+  if [[ "$arg" =~ '^--dry$|@Dry run, only echo commands' ]]; then local PREFIX=echo;continue;fi
+  set -- "$@" "$arg"
+done
+
+echo "Attempting to pull from ${source_dir}"
+
+source_files=$(adb shell find "${source_dir}" -type f)
+
+if ! (echo -e "${source_files}" | while read source_file;do 
+    echo "Pulling ${source_file} ..."
+    if [[ ! $($PREFIX adb pull "${source_file}" "${2}") ]];then
+      err=true
+    fi
+done);then 
+  err=true;
+fi
+
+if [[ -z $err ]];then
+  echo "Success: Pulled all files from ${source_dir}"
+  if [[ $PREFIX == 'echo' ]];then
+    echo "Operating in Dry Mode, nothing left to do"
+    return
+  fi
+  if confirm -p "Delete pulled files?";then
+    echo "Deleting files from ${source_dir}"
+    adb shell rm -f "${source_dir}/*"
+  fi
+else
+  echo "Failed: There was at least one error when pulling files from ${source_dir}"
+fi
+
+}
