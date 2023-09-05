44a7f006de1bd40649d7d5462976038cab4a356a
Bert Tejeda <etejeda@tecknicos.com>
2023-09-05 13:29:38 -0400

    Added device_id flag for adb.pull

--- android.functions.sh
+++ android.functions.sh
@@ -22,18 +22,19 @@ local PREFIX=eval
 for arg in "${@}";do
   shift
   if [[ "$arg" =~ '^--source-dir$|^-s$|@The source directory to pull files from - required' ]]; then local source_dir=$1;continue;fi
-  if [[ "$arg" =~ '^--destination_dir$|^-d$|@The destination directory to store the pulled files - required' ]]; then local clip_seek_time=$1;continue;fi
+  if [[ "$arg" =~ '^--destination_dir$|^-d$|@The destination directory to store the pulled files - required' ]]; then local destination_dir=$1;continue;fi
+  if [[ "$arg" =~ '^--device-id$|^-id$|@The android device id to target - required' ]]; then local android_device_id="${1}";continue;fi
   if [[ "$arg" =~ '^--dry$|@Dry run, only echo commands' ]]; then local PREFIX=echo;continue;fi
   set -- "$@" "$arg"
 done
 
 echo "Attempting to pull from ${source_dir}"
 
-source_files=$(adb shell find "${source_dir}" -type f)
+source_files=$(adb -s ${android_device_id} shell find "${source_dir}" -type f)
 
 if ! (echo -e "${source_files}" | while read source_file;do 
     echo "Pulling ${source_file} ..."
-    if [[ ! $($PREFIX adb pull "${source_file}" "${2}") ]];then
+    if [[ ! $($PREFIX adb -s ${android_device_id} pull "${source_file}" "${2}") ]];then
       err=true
     fi
 done);then 
@@ -48,7 +49,7 @@ if [[ -z $err ]];then
   fi
   if confirm -p "Delete pulled files?";then
     echo "Deleting files from ${source_dir}"
-    adb shell rm -f "${source_dir}/*"
+    adb -s ${android_device_id} shell rm -f "${source_dir}/*."
   fi
 else
   echo "Failed: There was at least one error when pulling files from ${source_dir}"
