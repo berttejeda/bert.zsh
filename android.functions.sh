a74ea5b0c65b1ebc3c8a80690a406492786f38f5
Bert Tejeda <berttejeda@gmail.com>
2025-11-24 10:10:44 -0500

    Adjusted pull function

--- android.functions.sh
+++ android.functions.sh
@@ -3,11 +3,11 @@ adb.install () { adb install $1 ;}
 adb.push () { adb push $1 $2 ;}
 
 android.backup.personal () { 
-	folder_root="/storage/emulated/0/"
-	folders=$(adb shell ls $folder_root | grep -ie 'dcim\|pictures\|video\|download')
-	for folder in $(echo -e "${folders}");do
-		echo adb shell "ls -R ${folder_root}${folder}" | grep -vi '.*thumb*' | tr '\r' ' ' | while read file; do adb pull $file;done
-	done
+  folder_root="/storage/emulated/0/"
+  folders=$(adb shell ls $folder_root | grep -ie 'dcim\|pictures\|video\|download')
+  for folder in $(echo -e "${folders}");do
+    echo adb shell "ls -R ${folder_root}${folder}" | grep -vi '.*thumb*' | tr '\r' ' ' | while read file; do adb pull $file;done
+  done
 }
 
 adb.pull () {
